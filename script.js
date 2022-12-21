7fa3633c75f173c8209b02208936ec346ca7d805
Jamie Goodson <jamie.goodson@tractable.ai>
2022-12-21 14:35:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function injectCopyBtns() {
+  const ticketIdContainerEls = document.getElementsByClassName("ghx-stat-2");
+
+  for (const ticketIdContainerEl of ticketIdContainerEls) {
+    const hasCopyBtn =
+      ticketIdContainerEl.querySelector('[aria-label="copy-btn"]') != null;
+    if (hasCopyBtn) continue;
+
+    const ticketIdEl = ticketIdContainerEl.getElementsByClassName("ghx-key")[0];
+    const ticketId = ticketIdEl.ariaLabel;
+    const copyBtn = createCopyBtn(ticketId);
+    ticketIdContainerEl.appendChild(copyBtn);
+  }
+}
+
+function createCopyBtn(ticketId) {
+  const copyBtn = document.createElement("span");
+  copyBtn.ariaLabel = "copy-btn";
+  copyBtn.className = "copy-btn material-symbols-outlined";
+  copyBtn.textContent = "content_copy";
+
+  copyBtn.onclick = (e) => {
+    e.stopPropagation();
+    navigator.clipboard.writeText(ticketId);
+    copyBtn.textContent = "done"; // Change to done icon
+
+    // Reset icon
+    setTimeout(() => {
+      copyBtn.textContent = "content_copy";
+    }, 1000);
+  };
+
+  return copyBtn;
+}
+
+setInterval(() => {
+  injectCopyBtns();
+}, 1000);
